2821040f8ebac76b0118fc2fea782a4ef9c8d77b
unknown <sabbir.cse@yahoo.com>
2021-02-07 19:34:36 +0600

    version 1.01

--- API-basic-concept/cooking-master/script.js
+++ API-basic-concept/cooking-master/script.js
@@ -22,7 +22,7 @@ function showMeal(data){
         const div = document.createElement('div');
         div.className = 'img-title' ;
         const mealInfo = `
-            <a href="#"><img onclick="getDetails('${mealsInfo.strMeal}')" src=${mealsInfo.strMealThumb}> </a>
+            <a href="#show-Ingredient"><img onclick="getDetails('${mealsInfo.strMeal}')" src=${mealsInfo.strMealThumb}> </a>
             <p class="title">${mealsInfo.strMeal}</p>
         `;
 
@@ -43,25 +43,29 @@ function getDetails(mealIngredient){
 
 function showIngredient(ingredients){
     const ingredientSection = document.getElementById('show-Ingredient');
+    const cookInfo = `
+            <div>
+                <img src="${ingredients.strMealThumb}" >
+                <h1 id="meal-name">${ingredients.strMeal}</h1>
+                <h5>Ingredients : </h5>
+                <ul id="list-inge">
+                
+                </ul>
+            </div>
+        `;
+    ingredientSection.innerHTML = cookInfo;
+
     for (let i = 1; i <= 20; i++) {  
         let ingredient = ingredients['strIngredient'+i.toString()];
         if(ingredient=='')
         {
             break;
         }  
-        const cookInfo = `
-            <div>
-                <img src="${ingredients.strMealThumb}" >
-                <h1 id="meal-name"></h1>
-                <h5>Ingredients : </h5>
-                <ul id="list-inge">
-                    <li>${ingredient}</li>
-                </ul>
-            </div>
-        `;
-        ingredientSection.innerHTML = cookInfo;
-
         // console.log( ingredient );
+        const ul = document.getElementById('list-inge');
+        const li = document.createElement('li');
+        li.innerText = ingredient;
+        ul.appendChild(li);
     }
    
 }
