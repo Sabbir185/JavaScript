5b869975766b73bc7df9a01d85d306740d8cd9da
unknown <sabbir.cse@yahoo.com>
2021-02-16 16:01:10 +0600

    type conversion

---
+++ advance-javascript/typeConversion.js
@@ -0,0 +1,32 @@
+// string to number conversion
+const name1 = '45';
+console.log(typeof(name1));
+
+
+const number1 = +'49' ;
+console.log(typeof(number1));
+
+
+const name5 = '23' ;
+const integerNum = parseInt(name5) ; // using method int
+console.log( typeof(integerNum) );
+
+
+const floatName = '3.1416' ;
+const floatNumber = parseFloat(floatName) ;
+console.log( typeof(floatNumber) );
+
+
+console.log('--------------------');
+
+
+// number to string
+const num = 23;
+console.log(typeof(num));
+
+const name2 = 23+'' ;
+console.log(typeof(name2));
+
+const n = 100 ;
+const m = n.toString();
+console.log( typeof(m) ); // using toString()
