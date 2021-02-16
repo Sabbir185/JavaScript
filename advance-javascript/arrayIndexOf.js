c68aaaf5bfca71a45eb35fa341e12fd5db0fd5bd
unknown <sabbir.cse@yahoo.com>
2021-02-16 12:21:27 +0600

    array indexOf

---
+++ advance-javascript/arrayIndexOf.js
@@ -0,0 +1,15 @@
+
+const word = ['h', 'e', 'l', 'l', 'o'];
+
+console.log('Index of e is : ', word.indexOf('e') );
+
+// by default count 1st item 
+console.log('Index of l is : ', word.indexOf('l') );
+
+// start from index 3
+console.log('Index of 2nd l is : ', word.indexOf('l',3) );
+
+// doesn't exit , -1
+console.log('Index of Y is : ', word.indexOf('Y') );
+
+
