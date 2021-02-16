6737cfbe4b66bc7117901a199d0da62d5e299ea2
unknown <sabbir.cse@yahoo.com>
2021-02-16 12:39:28 +0600

    more of indexOf

--- advance-javascript/arrayIndexOf.js
+++ advance-javascript/arrayIndexOf.js
@@ -12,4 +12,19 @@ console.log('Index of 2nd l is : ', word.indexOf('l',3) );
 // doesn't exit , -1
 console.log('Index of Y is : ', word.indexOf('Y') );
 
+//.............................................
+
+// Finding all the occurrences of an element
+
+const arr = ['a', 'b', 'c', 'a', 'd', 'a'];
+
+let index = arr.indexOf('a'); // output 0
+while( index != -1 ){
+    console.log(index);
+    index = arr.indexOf('a', ++index );
+}
+
+// console.log(arr.indexOf('a',6));  // loop break condition
+
+
 
