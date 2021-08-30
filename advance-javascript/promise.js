ceb5bbdbdb552be2c1d3636bbfe542f743655bbe
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-08-30 12:58:45 +0600

    js promise

---
+++ advance-javascript/promise.js
@@ -0,0 +1,21 @@
+// Real life
+// promise -> pending
+// pending -> successful OR failure
+
+// JavaScript Promise
+// Promise -> Async Operation (initial state: Pending)
+// Pending -> success --> resolved
+// Pending -> failure --> rejected
+
+const prom = new Promise((resolve, reject) => {
+    // async operation => result
+    resolve('The promise is success');
+    reject('The promise is fail!');
+})
+
+// initial pending
+console.log(prom)
+
+prom
+    .then(data => console.log(data)) // resolve
+    .catch(err => console.log(err)); // reject
